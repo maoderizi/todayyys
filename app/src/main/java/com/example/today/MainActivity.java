4ac1123ef9fc32fb56bde6e7d823d163997a6058
maoderizi <damdam2020@naver.com>
2021-03-24 21:37:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/today/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.today;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn; //왜안돼진짜...?
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button);
+        btn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show();
+            }
+
+        });
+    }
+    void show(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("안녕").setMessage("세계");
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+}
