b858eb4aada0e249536a504d56f6559afc26227a
maoderizi <damdam2020@naver.com>
2021-03-24 21:51:11 +0900

    이야야야앗

--- app/src/main/java/com/example/today/MainActivity.java
+++ app/src/main/java/com/example/today/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     }
     void show(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("안녕").setMessage("!!");
+        builder.setTitle("안녕").setMessage("!!!!!");
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
 
